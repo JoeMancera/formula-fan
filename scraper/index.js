4e6290d84d10cff08ef3fd8a4355e897f9541613
Joe Mancera <joeharrymancera@gmail.com>
2023-07-24 20:54:53 -0500

    add scraper files from old project

---
+++ scraper/index.js
@@ -0,0 +1,16 @@
+import { logInfo } from './log.js'
+import { scrapeAndSave, SCRAPINGS } from './utils.js'
+
+// get first parameter from command line
+const scrapeParameter = process.argv.at(-1)
+
+if (SCRAPINGS[scrapeParameter]) {
+  await scrapeAndSave(scrapeParameter)
+} else {
+  logInfo('Scraping all data...')
+
+  for (const infoToScrape of Object.keys(SCRAPINGS)) {
+    await scrapeAndSave(infoToScrape)
+  }
+
+}
