f5858d41e1ba9cbce894eb47a84950c551eda90a
Joe Mancera <joeharrymancera@gmail.com>
2023-11-27 20:17:24 -0500

    fix: show the last event for the current year

--- utils/schedule.ts
+++ utils/schedule.ts
@@ -4,9 +4,11 @@ import type { Event } from '../types/calendar'
 export const getNextEvent = async () => {
 	const events = (await readDBFile('schedule')) as Event[]
 	// get the current event from the array of events
-	return events.find((event) => {
+	const nextEvent = events.find((event) => {
 		const now = new Date()
 		const eventDate = new Date(event.dates.race)
 		return eventDate >= now
 	})
+
+	return nextEvent ? nextEvent : events[events.length - 1]
 }
