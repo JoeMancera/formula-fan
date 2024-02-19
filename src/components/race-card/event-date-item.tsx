020ee0aa54d5c106e627ab2be2d200b15214eaac
Joe Mancera <joeharrymancera@gmail.com>
2024-02-19 15:19:39 -0500

    chore: improve text wrap

--- src/components/race-card/event-date-item.tsx
+++ src/components/race-card/event-date-item.tsx
@@ -9,7 +9,7 @@ export const EventDateItem = ({ nameSession, date }: EventDateItemProps) => {
 	const hourDate = getHourFromDate({ date })
 	return (
 		<li className='font-semibold flex justify-between items-center px-2 py-1 border-b border-white border-opacity-40'>
-			<span className='' style={{ flex: '0 0 28%' }}>{nameSession}</span>
+			<span className='text-nowrap' style={{ flex: '0 0 28%' }}>{nameSession}</span>
 			<span className='text-success font-light'>{dayDate}</span>
 			<span className='text-orange'>{hourDate}</span>
 		</li>
