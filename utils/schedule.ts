0b9bc4fd91f7b55f4b50eaba780ca725b97e6f9c
Joe Mancera <joeharrymancera@gmail.com>
2024-03-30 06:22:32 -0500

    fix: add sorting for the events

--- utils/schedule.ts
+++ utils/schedule.ts
@@ -3,6 +3,12 @@ import type { Event } from '../types/calendar'
 
 export const getNextEvent = async () => {
 	const events = (await readDBFile('schedule')) as Event[]
+	//order the events by date
+	events.sort((a, b) => {
+		const dateA = new Date(a.dates.race)
+		const dateB = new Date(b.dates.race)
+		return dateA.getTime() - dateB.getTime()
+	})
 	// get the current event from the array of events
 	const nextEvent = events.find((event) => {
 		const now = new Date()
