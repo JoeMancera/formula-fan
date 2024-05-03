c12af117d15c3a6b3188b74a51ae3f6bb5bd00a8
Joe Mancera <joeharrymancera@gmail.com>
2024-05-03 08:31:24 -0500

    fix: change the selectors for drivers

--- scraper/championship.js
+++ scraper/championship.js
@@ -1,15 +1,15 @@
 const SELECTORS = {
-  seasonTitle: 'h1.f1-black--xxl.no-margin',
-  drivers: '.col-12.col-md-6.col-lg-4'
+  seasonTitle: 'title',
+  drivers: '.outline.outline-offset-4.outline-brand-black.group.outline-0'
 }
 
 const DRIVER = {
-  firstName: '.d-block.f1--xxs.f1-color--carbonBlack',
-  lastName: '.d-block.f1-bold--s.f1-color--carbonBlack',
-  url: 'a.listing-item--link',
-  team: '.listing-item--team.f1--xxs.f1-color--gray5',
-  points: '.points .f1-wide--s',
-  rank: '.rank'
+  firstName: 'p.f1-heading.tracking-normal.text-fs-12px.leading-tight.uppercase.font-normal.non-italic.f1-heading__body.font-formulaOne',
+  lastName: 'p.f1-heading.tracking-normal.text-fs-18px.leading-tight.uppercase.font-bold.non-italic.f1-heading__body.font-formulaOne',
+  url: 'href',
+  team: 'p.f1-heading.tracking-normal.text-fs-12px.leading-tight.normal-case.font-normal.non-italic.f1-heading__body.font-formulaOne.text-greyDark',
+  points: 'p.f1-heading-wide.font-formulaOneWide.tracking-normal.font-normal.non-italic.text-fs-18px.leading-none.normal-case',
+  rank: 'p.f1-heading-black.font-formulaOne.tracking-normal.font-black.non-italic.text-fs-24px'
 }
 
 export async function getChampionship($) {
@@ -20,7 +20,7 @@ export async function getChampionship($) {
   driversContent.each((_, driver) => {
     const driverFirstName = $(driver).find(DRIVER.firstName).text()
     const driverLastName = $(driver).find(DRIVER.lastName).text()
-    const driverUrl = $(driver).find(DRIVER.url).attr('href')
+    const driverUrl = $(driver).attr(DRIVER.url)
     const driverTeam = $(driver).find(DRIVER.team).text()
     const driverPoints = $(driver).find(DRIVER.points).text()
     const driverRank = $(driver).find(DRIVER.rank).text()
@@ -30,8 +30,8 @@ export async function getChampionship($) {
       lastName: driverLastName,
       url: driverUrl,
       team: driverTeam,
-      points: driverPoints,
-      rank: driverRank
+      points: Number(driverPoints),
+      rank: Number(driverRank)
     })
   })
   return {
